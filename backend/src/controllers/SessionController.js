a841185f9acc4a59678e591fe33718f22431dbcf
Gustavo Riffel <gustavoriffell@gmail.com>
2020-03-29 14:42:38 +0100

    Add validations

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -2,17 +2,17 @@ const connection = require('../database/connection');
 
 module.exports = {
     async create(request, response) {
-        const { id } = request.body;
+        const { id } = request.body; // id que foi passado
 
-        const ong = await connection('ongs')
-            .where('id', id)
-            .select('name')
-            .first();
+        const ong = await connection('ongs') // tabela ong
+            .where('id', id)        // onde id passado seja = id da tab
+            .select('name')  // selecionar o nome
+            .first(); // devolver o primeiro resultado
 
         if (!ong) {
-            return response.status(400).json({ error: 'No ONG found with this ID'});
+            return response.status(400).json({ error: 'No ONG found with this ID'}); // se o id nao existir na tab
         }
 
-        return response.json(ong);
+        return response.json(ong); // retorna nome da ong 
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate'); // usado para validacoes
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -7,15 +8,57 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/sessions', SessionController.create);
+routes.post('/sessions',celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    id: Joi.string().required(),
+  }),
+}),
+SessionController.create);
 
 routes.get('/ongs', OngController.index); // listar ongs
-routes.post('/ongs', OngController.create); // criar ongs
 
-routes.get('/profile', ProfileController.index);
+routes.post('/ongs', celebrate({  // criar ongs
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10).max(13),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2),
+  }),
+}),
+OngController.create); 
 
-routes.post('/incidents', IncidentController.create);
-routes.get('/incidents', IncidentController.index);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/profile', celebrate({  // login
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+}),
+ProfileController.index); 
+
+routes.post('/incidents',celebrate({  // criar caso
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    value: Joi.number().required(),
+  }),
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+}),
+IncidentController.create); 
+
+routes.get('/incidents', celebrate({  // listar caso
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number(),
+  }),
+}),
+IncidentController.index); 
+
+routes.delete('/incidents/:id', celebrate({  // deletar caso
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required(),
+  }),
+}),
+IncidentController.delete); 
 
 module.exports = routes;
